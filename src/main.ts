0db7d9482b0704c4a45e1a502d172ede718f1ff9
blacktunes <blacktunes@foxmail.com>
2024-07-17 18:42:51 +0800

    feat: 增加访问统计

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import './main.styl'
 
+import { analytics } from 'star-rail-vue'
 import VueDOMPurifyHTML from 'vue-dompurify-html'
 import App from './App.vue'
 import './assets/scripts/setup'
 import './assets/scripts/keyboard'
 
 window.BUILD_TIME = new Date(BUILD_TIME)
+
+analytics('G-YW1NH1GCBF', import.meta.env.MODE === 'development')
+
 createApp(App)
   .use(VueDOMPurifyHTML, {
     allowedTags: ['br']
